a38ec89b45315959304ccab742cf23e14b6c07a0
KruthiGS <63146460+KruthiGS@users.noreply.github.com>
2020-12-22 20:59:36 +0530

    Create lab6.php

---
+++ lab6.php
@@ -0,0 +1,12 @@
+<?php session_start();
+$fp = fopen("counter.txt","r");
+$count= fread($fp,1024);
+fclose($fp);
+
+$count=$count+1;
+echo "<p> Page views:".$count."</p>";
+$fp = fopen("counter.txt","w");
+fwrite($fp,$count);
+fclose($fp);
+?>
+© 2020 GitHub, Inc.
